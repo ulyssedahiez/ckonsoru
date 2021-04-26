9534acadb3b22e7f0cd1b378c40aa3a6d31df910
SimonBurdy <simon.burdy@lacatholille.fr>
2021-04-26 10:31:06 +0200

    date ok

--- src/main/java/com/fges/ckonsoru/App.java
+++ src/main/java/com/fges/ckonsoru/App.java
@@ -5,7 +5,17 @@
  */
 package com.fges.ckonsoru;
 
+
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Properties;
+
+
 
 
 
@@ -15,13 +25,23 @@ import java.util.Properties;
  * @author julie.jacques
  */
 public class App {
+	
+	
 
-    public static void main(String args[]){
+    public static void main(String args[]) throws ParseException{
         
-        Client monCli  = new Client("Durant","Paul");
+        Client monCli  = new Client(1,"Durant","Paul");
         monCli.info();
-        monCli.showVet();
+        
+        DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+        LocalDateTime debut = LocalDateTime.parse("05/05/2018 11:50", timeFormatter);
+        
+        System.out.println("Disponibilités pour le " + debut.format(timeFormatter));
 
+        
+      // Semaine maSem = new Semaine()
+      
+        
         System.out.println("Bienvenue sur Clinique Konsoru !");
         
         // chargement de la configuration de la persistence bonjour bonjour
--- src/main/java/com/fges/ckonsoru/Client.java
+++ src/main/java/com/fges/ckonsoru/Client.java
@@ -1,17 +1,31 @@
 package com.fges.ckonsoru;
 
+import java.sql.Statement;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Properties;
+
 
 
 public class Client {
     public int id;
     public String nom;
     public String prenom;
+    
+    public ConfigLoader maConf = new ConfigLoader();
+    public Properties prop = maConf.getProperties();
+	
+    public String url = prop.getProperty("bdd.url");
+    public String login = prop.getProperty("bdd.login");
+    public String mdp = prop.getProperty("bdd.mdp");
+    
+    
 
-    public Client(String name ,String firstname ){
+    public Client(int id ,String name ,String firstname ){
+    	this.id =  id;
         this.nom = name;
         this.prenom = firstname;
         
@@ -21,36 +35,23 @@ public class Client {
     public void info(){
         System.out.println( "Nom : "+ this.nom + " ,Prenom :" + this.prenom);
     }
-    /*bdd.url=jdbc:postgresql://localhost/ckonsoru
-    bdd.login=postgres
-    bdd.mdp=$im1789Jps*/
 
 
 
-    public void showVet(){
-       
-        Connection conn1 = null;
-        String dbURL1 = "jdbc:postgresql://localhost:5432/ckonsoru";
-        String username = "postgre";
-        String password = "$im1789Jps";
+
+    /*public void showVet(){
+        Connection conn1 = null;        
         try{
-            conn1 = DriverManager.getConnection(dbURL1 , username,password);
-            System.out.println("Connected to database #1");
-            conn1.close();
+        	
+            conn1 = DriverManager.getConnection(url , login,mdp);
+            System.out.println("Connected to database #1");    
+            conn1.close();       
         }catch(SQLException e){
             System.out.println("Sa merde gros #1");
             e.printStackTrace();
 
         }
-        
-        
-                
-           
-              
-           
-        
-
-    }
+    }*/
 
 
 
