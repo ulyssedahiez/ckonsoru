713ac098ebeee6c0ef8a2b152a6686d73d1f0dae
DahiezU <62097018+DahiezU@users.noreply.github.com>
2021-05-01 21:58:21 +0200

    xml

--- src/main/java/com/fges/ckonsoru/App.java
+++ src/main/java/com/fges/ckonsoru/App.java
@@ -7,6 +7,7 @@ package com.fges.ckonsoru;
 
 
 
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.DriverManager;
@@ -20,8 +21,13 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
 import java.util.Properties;
 import java.util.Scanner;
+
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.xml.sax.SAXException;
 
 
 
@@ -41,50 +47,10 @@ public class App {
 	
 
 
-    public static void main(String args[]) throws ParseException{
-        
-       /* Client monCli  = new Client(1,"Durant","Paul");
-        monCli.info();*/
-        
-        
-        //<DATE ESSAI 
-        /*DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
-        LocalDateTime debut = LocalDateTime.parse("18/03/2021 11:50", timeFormatter);*/
-        
-        
-        
-        //lundi["8:00","17:00"]
-        
-        
-        
-        //System.out.println("Disponibilités pour le " + debut.format(timeFormatter));
-        
-        /*Veterinaire vet = new Veterinaire(1,"L","Belacqua");
-        vet.creationSemaine(1);*/
-        /*public Semaine(int id, String firstname, String lastname , Date lundi[] , Date mardi[] , 
-    			Date mercredi[], Date jeudi[], Date vendredi[], Date samedi[])*/
-        
-        //String[] lbLundi = {"15:00","19:00"};
-        /*String[] lbMardi = {"8:00","12:00"};
-        String[] lbMercredi = {"",""};
-        String[] lbJeudi = {"8:00","12:00"};
-        String[] lbVendredi = {"",""};
-        String[] lbSamedi = {"8:00","12:00"};*/
-        
-        
-        //Semaine maSem = new Semaine(1,"L","Belacqua",lbLundi,lbMardi , lbMercredi, lbJeudi, lbVendredi, lbSamedi) ;
-        
-       // maSem.info();
-        
-        
-        
-        ///creationSemaine(1);
+    public static void main(String args[]) throws ParseException, ParserConfigurationException, SAXException, IOException{
+   
         System.out.println("Bienvenue sur Clinique Konsoru !");
         
-        System.out.println(System.currentTimeMillis());
-        System.out.println(new java.util.Date());
-        System.out.println(Calendar.getInstance().getTimeInMillis() );
-        System.out.println(Calendar.getInstance().getTime().getTime ());
         
         // chargement de la configuration de la persistence
         ConfigLoader cf = new ConfigLoader();
@@ -107,102 +73,21 @@ public class App {
         System.out.println("Entrer un numéro d action:");
 
         
-
-        Scanner scanIn = new Scanner(System.in);
-        int numero = scanIn.nextInt();
-        ConnexionBDD test = new ConnexionBDD();
-        while(numero != 9){
-
+        ConnexionXML test = new ConnexionXML();
+        //test.mainXml();
         
-            
-            switch(numero) {
-                case 1:
-                    
-                    String explicationStrDispo = "Entrer une date au format JJ/MM/AAAA (ex: 18/03/2021) :";
-                    System.out.println(explicationStrDispo);
-                    
-                    Scanner scannCrenaux = new Scanner(System.in);
-                    String DateIn =  scannCrenaux.nextLine();
-                    
-
-                    DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
-                    LocalDateTime debut = LocalDateTime.parse(DateIn+" 11:50", timeFormatter);
-                    test.AffichageDispoCorrect(test.dispoAllVet(debut) , debut);
-                    //scannCrenaux.close();
-
-                    System.out.println(choix);
-                    System.out.println("Entrer un numéro d action:");
-                    numero = scanIn.nextInt();
-                break;
-                case 2:
-                    String explicationStrRdvCli = "Affichage des rendez-vous d un client \n" + "Indiquer le nom du client";
-                    System.out.println(explicationStrRdvCli);
-                    Scanner scanNom = new Scanner(System.in);
-                    String nomCli =  scanNom.nextLine();
-                    test.rdvClientAfficher(nomCli);
-
-                    System.out.println(choix);
-                    System.out.println("Entrer un numéro d action:");
-                    numero = scanIn.nextInt();
-                break;
-                case 3:
-                    String explicationStrPriseRdv = "Prise de rendez-vous \n" + "Indiquer une date et heure de début au format JJ/MM/AAAA HH:MM (ex: 18/03/2021 15:00)";
-                    System.out.println(explicationStrPriseRdv);
-                    
-                    Scanner scanDateRdv = new Scanner(System.in);
-                    String dateRdv =  scanDateRdv.nextLine();
-
-                    System.out.println("ndiquer le nom du vétérinaire" );                   
-                    Scanner scanNomRdv = new Scanner(System.in);
-                    String nomVetRdv =  scanNomRdv.nextLine();
-
-                    System.out.println("Indiquer le nom du client");
-                    Scanner scanNomCliRdv = new Scanner(System.in);
-                    String nomCliRdv =  scanNomCliRdv.nextLine();
-                    
-                    
-                    //String essai = "27/05/2021 08:20";
-                    //test.priseRdv(essai,"L. Belacqua","B. Costa");
-                    test.priseRdv(dateRdv,nomVetRdv,nomCliRdv);
-                    System.out.println(choix);
-                    System.out.println("Entrer un numéro d action:");
-                    numero = scanIn.nextInt();
-                break;
-                case 4:
-                    String explicationSupprimerRdv = "Indiquer une date et heure de début au format JJ/MM/AAAA HH:MM (ex: 18/03/2021 15:00)";
-                    System.out.println(explicationSupprimerRdv);
-                    
-                    Scanner scanDateCliSupprRDV = new Scanner(System.in);
-                    String DateRdvsupp = scanDateCliSupprRDV.nextLine();
-
-                    System.out.println("Indiquer le nom du client");
-                    Scanner scanNomCliSupprRDV = new Scanner(System.in);
-                    String nomCliRdvsupp = scanNomCliSupprRDV.nextLine();
-
-
-                    //String essai = "27/05/2021 08:20";
-                    
-                    test.supprRdv(DateRdvsupp ,nomCliRdvsupp );
-                    
-                    System.out.println(choix);
-                    System.out.println("Entrer un numéro d action:");
-                    numero = scanIn.nextInt();
-                break;
-                case 9:
-                    System.exit(0);
-                    break;
-                default:
-                    System.out.println("Excusez moi je n'ai pas compris \n");
-                    System.out.println(choix);
-                    System.out.println("Entrer un numéro d action:");
-                    numero = scanIn.nextInt();
-            }
-
-        }
-        scanIn.close();
-
+        DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+        LocalDateTime debut = LocalDateTime.parse("18/03/2021 11:50", timeFormatter);
         
-        //System.out.println(sWhatever);
+        test.getDisponibilites("L. Asriel", debut);
+        
+        //System.out.println(test.getDay(debut));
+        
+        
+        
+        //System.out.println(test.getRDVVeto("L. Asriel", debut));
+        
+        
         
         
     }
--- src/main/java/com/fges/ckonsoru/ConnexionXML.java
+++ src/main/java/com/fges/ckonsoru/ConnexionXML.java
@@ -1,5 +1,203 @@
 package com.fges.ckonsoru;
 
-public class ConnexionXML {
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.DocumentBuilder;  
+import org.w3c.dom.Document;  
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+import org.w3c.dom.Node;  
+import org.w3c.dom.Element;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.time.DayOfWeek;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;  
+
+public class ConnexionXML {
+	File file = new File("C:\\Users\\dahie\\Documents\\SDN-S6\\design_pattern\\ckonsoru\\src\\main\\resources\\ckonsoru.xml");
+	
+	public void searchDidponibilite (LocalDateTime Date) throws SAXException, IOException {
+		
+	}
+	public ArrayList<String> getDisponibilites(String veterinaire, LocalDateTime Date) throws SAXException, IOException {
+		int Yout = 0;
+		
+		ArrayList<String> Disponibilites = new ArrayList<String>();
+		
+		 String LeJour = Date.toString().substring(8, 10);
+	        String LeMois = Date.toString().substring(5, 7);
+	        String LAnnee = Date.toString().substring(0,4);
+	       
+		
+		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();  
+		//an instance of builder to parse the specified xml file  
+		DocumentBuilder db;
+		try {
+			db = dbf.newDocumentBuilder();
+			Document doc = db.parse(file); 
+			doc.getDocumentElement().normalize();  
+			NodeList NodeList = doc.getElementsByTagName("debut");
+			
+			
+			NodeList Journee = doc.getElementsByTagName("jour");
+			
+			
+			for (int i = 0; i <Journee.getLength() ; i++) {
+				
+				
+				
+					if(Journee.item(i).getTextContent().equals(this.getDay(Date))) {
+						
+						
+						String Dispo = doc.getElementsByTagName("debut").item(i).getTextContent();
+						String Veterinaire = doc.getElementsByTagName("veterinaire").item(i).getTextContent();
+						String Heure = "";
+						String Minutes = "";
+						Yout = 0;
+					do {
+
+						
+						Heure = Dispo.substring(0,2);
+						Minutes = Dispo.substring(3,5);
+						
+						if(Yout != 0) {
+						Minutes = String.valueOf(Integer.parseInt (Minutes) + 20);
+						
+						}else {Yout = 1;}
+		
+						if(Minutes.equals("60")) {
+							Minutes = "00";
+							
+							Heure = String.valueOf(Integer.parseInt (Heure) + 1);
+							
+							if(Integer.parseInt (Heure) < 10) {
+								Heure = "0" + Heure;
+							}
+						}
+						
+						Dispo = Heure+":"+ Minutes;
+						String Ligne = Veterinaire + " : " + LeJour+"/"+LeMois+"/"+LAnnee + " " + Dispo;
+						if(Veterinaire.equals(veterinaire)){
+							
+						Disponibilites.add(Ligne);
+						
+						
+					}
+				    } while (Dispo.equals(doc.getElementsByTagName("fin").item(i).getTextContent()) == false);
+						
+					}
+			} 
+				
+		} catch (ParserConfigurationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}  
+		Disponibilites.remove(Disponibilites.size()-1);
+		System.out.println(Disponibilites);
+		return Disponibilites;
+		
+	}
+	
+	public ArrayList<String> getRDVVeto(String veterinaire, LocalDateTime dateRDV) {
+		
+		ArrayList<String> RendezVous = new ArrayList<String>();
+		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();  
+		//an instance of builder to parse the specified xml file  
+		DocumentBuilder db;
+		try {
+			db = dbf.newDocumentBuilder();
+			Document doc = db.parse(file); 
+			doc.getDocumentElement().normalize();  
+			
+			NodeList RDV = doc.getElementsByTagName("rdv");
+			Node DebutRendezVous = (Node) RDV.item(0).getChildNodes();
+			
+			
+			
+			String veto;
+			String ligne = null;
+			for (int i = 0; i <RDV.getLength() ; i++) {
+				
+				
+				
+				DebutRendezVous = (Node) RDV.item(i).getChildNodes();
+				
+				veto = DebutRendezVous.getTextContent().toString();
+				
+				String[] parts = veto.split("\\n");
+				String datedure = parts[1];
+				String clientdure = parts[2];
+				String vetodure = parts[3];
+				
+				
+				if(vetodure.equals(veterinaire)) {
+				
+				String LeJour = datedure.substring(8, 10);
+		        String LeMois = datedure.substring(5, 7);
+		        String LAnnee = datedure.substring(0,4);
+		        String Heure = datedure.substring(11,13);
+				String Minutes = datedure.substring(14,16);
+				
+				String Complet = vetodure+" : "+LeJour+"/"+LeMois+"/"+LAnnee+" "+Heure+":"+Minutes;
+				//System.out.println(Complet);
+				RendezVous.add(Complet);
+				}
+			}
+			
+						
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		
+		
+		return RendezVous;
+	}
+	
+	public String getDay(LocalDateTime dateDispo) {
+		int dayofweek = dateDispo.getDayOfWeek().getValue();
+		String jour = "";
+		if(dayofweek == 1) {
+			jour = "lundi";
+		}
+		if(dayofweek == 2) {
+			jour = "mardi";
+		}
+		if(dayofweek == 3) {
+			jour = "mercredi";
+		}
+		if(dayofweek == 4) {
+			jour = "jeudi";
+		}
+		if(dayofweek == 5) {
+			jour = "vendredi";
+		}
+		if(dayofweek == 6) {
+			jour = "samedi";
+		}
+		if(dayofweek == 7) {
+			jour = "dimanche";
+		}
+		return jour;
+	}
+	
+	
+	
+	public void mainXml () throws ParserConfigurationException, SAXException, IOException {
+		
+	 
+		
+	
+	
+	
+	
+	
+	}
+	
 }
+
